571157bacce3e2d98c9a09b172c7aa3ccecbfdd8
Bas Man <bas.man.developer@gmail.com>
2023-05-06 14:38:48 +0100

    section 4

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { BasicTable } from './components/BasicTable';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <BasicTable />
+      </div>
+    );
+  }
+}
+
+export default App;
